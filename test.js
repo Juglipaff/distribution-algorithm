fe5499f2a51a2d3fa61123b76e88837c6a58d529
Juglipaff <juglipaff@gmail.com>
2022-04-27 10:07:14 +0300

    add sum of rewards checks

--- test.js
+++ test.js
@@ -14,7 +14,8 @@ describe('deposits', () => {
         const amount = 1000
         c.deposit(userA, amount, 0)
         c.setExpectedReward(100, 100)
-        expect(c.userBalance(userA)).toBeCloseTo(amount, 1)
+        expect(c.userBalance(userA)).toBeCloseTo(amount, 8)
+        expect(c.userBalance(userA)).toBeCloseTo(c.getTotalDeposits(), 8)
     })
 
     test('deposits tokens for multiple users', () => {
@@ -29,6 +30,9 @@ describe('deposits', () => {
         expect(c.userBalance(userA)).toBeCloseTo(amount1, 1)
         expect(c.userBalance(userB)).toBeCloseTo(amount2, 1)
         expect(c.userBalance(userC)).toBeCloseTo(amount3, 1)
+
+        expect(c.userBalance(userA) + c.userBalance(userB) + c.userBalance(userC)).toBeCloseTo(amount1 + amount2 + amount3, 8)
+        expect(c.userBalance(userA) + c.userBalance(userB) + c.userBalance(userC)).toBeCloseTo(c.getTotalDeposits(), 8)
     })
 
     test('deposits add up', () => {
@@ -52,6 +56,9 @@ describe('deposits', () => {
         expect(c.userBalance(userA)).toBeCloseTo(amount1 + amount4, 1)
         expect(c.userBalance(userB)).toBeCloseTo(amount2 + amount5, 1)
         expect(c.userBalance(userC)).toBeCloseTo(amount3 + amount6, 1)
+
+        expect(c.userBalance(userA) + c.userBalance(userB) + c.userBalance(userC)).toBeCloseTo(amount1 + amount2 + amount3 + amount4 + amount5 + amount6, 8)
+        expect(c.userBalance(userA) + c.userBalance(userB) + c.userBalance(userC)).toBeCloseTo(c.getTotalDeposits(), 8)
     })
 })
 
@@ -63,6 +70,7 @@ describe('withdraws', () => {
         c.setExpectedReward(50, 100)
         c.withdraw(userA, withdraw_amount1, 50)
         expect(c.userBalance(userA)).toBeCloseTo(amount1 - withdraw_amount1, 1)
+        expect(c.userBalance(userA)).toBeCloseTo(c.getTotalDeposits(), 8)
     })
 
     test('withdraws tokens for multiple users', () => {
@@ -85,6 +93,9 @@ describe('withdraws', () => {
         expect(c.userBalance(userA)).toBeCloseTo(amount1 - withdraw_amount1, 1)
         expect(c.userBalance(userB)).toBeCloseTo(amount2 - withdraw_amount2, 1)
         expect(c.userBalance(userC)).toBeCloseTo(amount3 - withdraw_amount3, 1)
+
+        expect(c.userBalance(userA) + c.userBalance(userB) + c.userBalance(userC)).toBeCloseTo(amount1 + amount2 + amount3 - withdraw_amount1 - withdraw_amount2 - withdraw_amount3, 8)
+        expect(c.userBalance(userA) + c.userBalance(userB) + c.userBalance(userC)).toBeCloseTo(c.getTotalDeposits(), 8)
     })
 })
 
@@ -110,6 +121,8 @@ describe('distributes', () => {
 
         expect(c.userBalance(userA)).toBeCloseTo(amount1 + reward*userADepositAge/totalDepositAge, 1)
         expect(c.userBalance(userB)).toBeCloseTo(amount2 + reward*userBDepositAge/totalDepositAge, 1)
+        expect(c.userBalance(userA) + c.userBalance(userB)).toBeCloseTo(amount1 + amount2 + reward, 8)
+        expect(c.userBalance(userA) + c.userBalance(userB)).toBeCloseTo(c.getTotalDeposits(), 8)
     })
 
     test('distributes tokens proportionally if invested different amounts on the same block', () => {
@@ -133,6 +146,8 @@ describe('distributes', () => {
 
         expect(c.userBalance(userA)).toBeCloseTo(amount1 + reward*userADepositAge/totalDepositAge, 1)
         expect(c.userBalance(userB)).toBeCloseTo(amount2 + reward*userBDepositAge/totalDepositAge, 1)
+        expect(c.userBalance(userA) + c.userBalance(userB)).toBeCloseTo(amount1 + amount2 + reward, 8)
+        expect(c.userBalance(userA) + c.userBalance(userB)).toBeCloseTo(c.getTotalDeposits(), 8)
     })
 
     test('distributes tokens proportionally if invested the same amount on different blocks',()=>{
@@ -157,6 +172,8 @@ describe('distributes', () => {
 
         expect(c.userBalance(userA)).toBeCloseTo(amount1 + reward*userADepositAge/totalDepositAge, 1)
         expect(c.userBalance(userB)).toBeCloseTo(amount2 + reward*userBDepositAge/totalDepositAge, 1)
+        expect(c.userBalance(userA) + c.userBalance(userB)).toBeCloseTo(amount1 + amount2 + reward, 8)
+        expect(c.userBalance(userA) + c.userBalance(userB)).toBeCloseTo(c.getTotalDeposits(), 8)
     })
 
     test('distributes tokens proportionally if invested on different blocks and had multiple deposits from the same user',()=>{
@@ -184,6 +201,8 @@ describe('distributes', () => {
 
         expect(c.userBalance(userA)).toBeCloseTo(amount1 + amount3 + reward*userADepositAge/totalDepositAge, 1)
         expect(c.userBalance(userB)).toBeCloseTo(amount2 + reward*userBDepositAge/totalDepositAge, 1)
+        expect(c.userBalance(userA) + c.userBalance(userB)).toBeCloseTo(amount1 + amount2 + amount3 + reward, 8)
+        expect(c.userBalance(userA) + c.userBalance(userB)).toBeCloseTo(c.getTotalDeposits(), 8)
     })
 })
 
@@ -217,6 +236,10 @@ describe('rewards distributed correctly', () => {
             expect(c.userBalance(userA)).toBeCloseTo(amount1 + amount3 + reward*userADepositAge/totalDepositAge, 1)
             expect(c.userBalance(userB)).toBeCloseTo(amount2 + reward*userBDepositAge/totalDepositAge, 1)
         })
+        test('sum of tokens is consistent',()=>{
+            expect(c.userBalance(userA) + c.userBalance(userB)).toBeCloseTo(amount1 + amount2 + amount3 + reward, 8)
+            expect(c.userBalance(userA) + c.userBalance(userB)).toBeCloseTo(c.getTotalDeposits(), 8)
+        })
     })
     describe('rewards distributed correctly after withdrawal with multiple deposits on the same block',()=>{
         const amount1 = 1000
@@ -247,6 +270,10 @@ describe('rewards distributed correctly', () => {
             expect(c.userBalance(userA)).toBeCloseTo(amount1 - amount3 + reward*userADepositAge/totalDepositAge, 1)
             expect(c.userBalance(userB)).toBeCloseTo(amount2 + reward*userBDepositAge/totalDepositAge, 1)
         })
+        test('sum of tokens is consistent',()=>{
+            expect(c.userBalance(userA) + c.userBalance(userB)).toBeCloseTo(amount1 + amount2 - amount3 + reward, 8)
+            expect(c.userBalance(userA) + c.userBalance(userB)).toBeCloseTo(c.getTotalDeposits(), 8)
+        })
     })
     describe('rewards distributed correctly after withdrawal with multiple deposits on different blocks', () => {
         const amount1 = 1000
@@ -276,6 +303,10 @@ describe('rewards distributed correctly', () => {
         test('doesnt affect deposits',()=>{
             expect(c.userBalance(userA)).toBeCloseTo(amount1 + reward*userADepositAge/totalDepositAge, 1)
             expect(c.userBalance(userB)).toBeCloseTo(amount2 - amount3 + reward*userBDepositAge/totalDepositAge, 1)
+        })
+        test('sum of tokens is consistent',()=>{
+            expect(c.userBalance(userA) + c.userBalance(userB)).toBeCloseTo(amount1 + amount2 - amount3 + reward, 8)
+            expect(c.userBalance(userA) + c.userBalance(userB)).toBeCloseTo(c.getTotalDeposits(), 8)
         })
     })
     describe('rewards distributed correctly after multiple withdrawals and deposits on different blocks', () => {
@@ -310,6 +341,10 @@ describe('rewards distributed correctly', () => {
         test('doesnt affect deposits',()=>{
             expect(c.userBalance(userA)).toBeCloseTo(amount1 - amount2 + amount3 + reward*userADepositAge/totalDepositAge, 1)
             expect(c.userBalance(userB)).toBeCloseTo(amount4 + reward*userBDepositAge/totalDepositAge, 1)
+        })
+        test('sum of tokens is consistent',()=>{
+            expect(c.userBalance(userA) + c.userBalance(userB)).toBeCloseTo(amount1 - amount2 + amount3 + amount4 + reward, 8)
+            expect(c.userBalance(userA) + c.userBalance(userB)).toBeCloseTo(c.getTotalDeposits(), 8)
         })
     })
     
@@ -361,6 +396,10 @@ describe('rewards distributed correctly', () => {
         test('doesnt affect deposits',()=>{
             expect(c.userBalance(userA)).toBeCloseTo(amount1 - amount4 + amount7 + reward*userADepositAge/totalDepositAge, 1)
             expect(c.userBalance(userB)).toBeCloseTo(amount2 - amount3 - amount5 + amount6 + reward*userBDepositAge/totalDepositAge, 1)
+        })
+        test('sum of tokens is consistent',()=>{
+            expect(c.userBalance(userA) + c.userBalance(userB)).toBeCloseTo(amount1 + amount2 - amount3 - amount4 - amount5 + amount6 + amount7 + reward, 8)
+            expect(c.userBalance(userA) + c.userBalance(userB)).toBeCloseTo(c.getTotalDeposits(), 8)
         })
     })
 })
@@ -447,7 +486,8 @@ describe('random scenarios', () => {
             expect(rewardD1).toBeCloseTo(0, 1)
         })
         test('sum of rewards after the first distribution is consistent',()=>{
-            expect(rewardA1 + rewardB1 + rewardC1 + rewardD1).toBeCloseTo(totalDeposits1, 1)
+            expect(rewardA1 + rewardB1 + rewardC1 + rewardD1).toBeCloseTo(totalDeposits1, 8)
+            expect(rewardA1 + rewardB1 + rewardC1 + rewardD1).toBeCloseTo(amount1 + amount2 + amount3 + reward1, 8)
         })
 
         test('rewards after the second distribution are correct',()=>{
@@ -457,7 +497,8 @@ describe('random scenarios', () => {
             expect(rewardD2).toBeCloseTo(amount7 + reward2*userDDepositAge2/totalDepositAge2, 1)
         })
         test('sum of rewards after the second distribution is consistent',()=>{
-            expect(rewardA2 + rewardB2 + rewardC2 + rewardD2).toBeCloseTo(totalDeposits2, 1)
+            expect(rewardA2 + rewardB2 + rewardC2 + rewardD2).toBeCloseTo(totalDeposits2, 8)
+            expect(rewardA2 + rewardB2 + rewardC2 + rewardD2).toBeCloseTo(amount1 + amount2 + amount3 + reward1 - amount5 + amount6 + amount7 + reward2, 8)
         })
     })
 
@@ -469,6 +510,7 @@ describe('random scenarios', () => {
         c.distribute(1000, 50)
 
         expect(c.userBalance(userA)).toBeCloseTo(1200, 1)
+        expect(c.userBalance(userA)).toBeCloseTo(c.getTotalDeposits(), 8)
     })
 
     test('rewards distributed correctly if multiple actions happened on the same block',()=>{
@@ -515,7 +557,8 @@ describe('random scenarios', () => {
         const userBDepositAge = (block8 - block2)*amount2
         const totalDepositAge = userADepositAge + userBDepositAge
 
-        expect(c.userBalance(userA) + c.userBalance(userB)).toBeCloseTo(c.getTotalDeposits(), 1)
+        expect(c.userBalance(userA) + c.userBalance(userB)).toBeCloseTo(c.getTotalDeposits(), 8)
+        expect(c.userBalance(userA) + c.userBalance(userB)).toBeCloseTo(amount1 + amount2 - amount3 - amount4 + amount5 - amount6 + amount7 + reward, 8)
         expect(c.userBalance(userA)).toBeCloseTo(amount1 - amount3 - amount4 + amount5 - amount6 + amount7 + reward*userADepositAge/totalDepositAge, 1)
         expect(c.userBalance(userB)).toBeCloseTo(amount2 + reward*userBDepositAge/totalDepositAge, 1)
     })
@@ -564,7 +607,8 @@ describe('random scenarios', () => {
         })
 
         test('sum of stakes is consistent',()=>{
-            expect(c.userBalance(userA) + c.userBalance(userB) + c.userBalance(userC)).toBeCloseTo(c.getTotalDeposits(), 1)
+            expect(c.userBalance(userA) + c.userBalance(userB) + c.userBalance(userC)).toBeCloseTo(c.getTotalDeposits(), 8)
+            expect(c.userBalance(userA) + c.userBalance(userB) + c.userBalance(userC)).toBeCloseTo(amount1 + amount2 + amount3 - amount4 + amount5 - amount6 + amount7 - amount8 + reward, 8)
         })
         test('doesnt affect rewards',()=>{
             expect(c.userBalance(userA)).toBeCloseTo(amount1 - amount4 + amount5 - amount8 + reward*userADepositAge/totalDepositAge, 1)
@@ -632,7 +676,8 @@ describe('random scenarios', () => {
             c.distribute(reward2, block12)      // 1000, 100
         })
         test('sum of stakes is consistent',()=>{
-            expect(c.userBalance(userA) + c.userBalance(userB) + c.userBalance(userC) + c.userBalance(userD)).toBeCloseTo(c.getTotalDeposits(), 1)
+            expect(c.userBalance(userA) + c.userBalance(userB) + c.userBalance(userC) + c.userBalance(userD)).toBeCloseTo(c.getTotalDeposits(), 8)
+            expect(c.userBalance(userA) + c.userBalance(userB) + c.userBalance(userC) + c.userBalance(userD)).toBeCloseTo(amount1 + amount2 + amount3 - amount4 + amount5 - amount6 + amount7 - amount8 + reward1 + amount10 + amount11 + reward2, 8)
         })
         test('doesnt affect rewards',()=>{
             expect(c.userBalance(userA)).toBeCloseTo(amount1 - amount4 + amount5 - amount8 + reward1*userADepositAge1/totalDepositAge1 + reward2*userADepositAge2/totalDepositAge2, 1)
@@ -675,7 +720,8 @@ describe('random scenarios', () => {
         })
 
         test('sum of stakes is consistent',()=>{
-            expect(c.userBalance(userA) + c.userBalance(userB)).toBeCloseTo(c.getTotalDeposits(), 1)
+            expect(c.userBalance(userA) + c.userBalance(userB)).toBeCloseTo(c.getTotalDeposits(), 8)
+            expect(c.userBalance(userA) + c.userBalance(userB)).toBeCloseTo(amount1 + reward1 + amount3 + reward2, 8)
         })
     })
     describe('rewards distributed correctly with redeposit with larger distribution block',()=>{
@@ -712,7 +758,8 @@ describe('random scenarios', () => {
         })
 
         test('sum of stakes is consistent',()=>{
-            expect(c.userBalance(userA) + c.userBalance(userB)).toBeCloseTo(c.getTotalDeposits(), 1)
+            expect(c.userBalance(userA) + c.userBalance(userB)).toBeCloseTo(c.getTotalDeposits(), 8)
+            expect(c.userBalance(userA) + c.userBalance(userB)).toBeCloseTo(amount1 + reward1 + amount3 + reward2, 8)
         })
     })
     describe('rewards distributed correctly after multiple distributions for one user',()=>{
@@ -735,6 +782,7 @@ describe('random scenarios', () => {
         })
         test('doesnt affect deposits',()=>{
             expect(c.userBalance(userA)).toBeCloseTo(5000, 1)
+            expect(c.userBalance(userA)).toBeCloseTo(c.getTotalDeposits(), 8)
         })
     })
 
@@ -797,7 +845,8 @@ describe('random scenarios', () => {
             expect(c.userBalance(userC)).toBeCloseTo(amount3 + reward1*userCDepositAge1/totalDepositAge1 + reward2*userCDepositAge2/totalDepositAge2 + reward3*userCDepositAge3/totalDepositAge3 + reward4*userCDepositAge4/totalDepositAge4, 1)
         })
         test('sum of balances is consistent',()=>{
-            expect(c.userBalance(userA) + c.userBalance(userB) + c.userBalance(userC)).toBeCloseTo(amount1 + amount2 + amount3 + reward1 + reward2 + reward3 + reward4, 1)
+            expect(c.userBalance(userA) + c.userBalance(userB) + c.userBalance(userC)).toBeCloseTo(amount1 + amount2 + amount3 + reward1 + reward2 + reward3 + reward4, 8)
+            expect(c.userBalance(userA) + c.userBalance(userB) + c.userBalance(userC)).toBeCloseTo(c.getTotalDeposits(), 8)
         })
     })
     describe('rewards distributed correctly if no distributions were made for one of the users', ()=>{
@@ -813,7 +862,8 @@ describe('random scenarios', () => {
             expect(c.userBalance(userB)).toBeCloseTo(100, 1)
         })
         test('doesnt affect deposits',()=>{
-            expect(c.userBalance(userA) + c.userBalance(userB)).toBeCloseTo(300, 1)
+            expect(c.userBalance(userA) + c.userBalance(userB)).toBeCloseTo(300, 8)
+            expect(c.userBalance(userA) + c.userBalance(userB)).toBeCloseTo(c.getTotalDeposits(), 8)
         })
     })
     describe('rewards distributed correctly if one of the users deposited on the distribution block', ()=>{
@@ -848,7 +898,82 @@ describe('random scenarios', () => {
             expect(c.userBalance(userB)).toBeCloseTo(amount2 + reward1*userBDepositAge1/totalDepositAge1 + reward2*userBDepositAge2/totalDepositAge2, 1)
         })
         test('doesnt affect deposits',()=>{
-            expect(c.userBalance(userA) + c.userBalance(userB)).toBeCloseTo(amount1 + amount2 + reward1 + reward2, 1)
+            expect(c.userBalance(userA) + c.userBalance(userB)).toBeCloseTo(amount1 + amount2 + reward1 + reward2, 8)
+            expect(c.userBalance(userA) + c.userBalance(userB)).toBeCloseTo(c.getTotalDeposits(), 8)
+        })
+    })
+
+
+    describe('sum of tokens is consistent after wrong ExpectedRewards',()=>{
+        const amount1 = 500
+        const amount2 = 700
+        const amount3 = 200
+        const reward1 = 2000
+
+        const block1 = 20
+        const block2 = 40
+        const block3 = 45
+        const block4 = 50
+
+        let rewardA1 = 0
+        let rewardB1 = 0
+        let rewardC1 = 0
+        let rewardD1 = 0
+        let totalDeposits1 = 0
+
+        const amount5 = 200
+        const amount6 = 200
+        const amount7 = 1000
+        const reward2 = 3000
+
+        const block5 = 60
+        const block6 = 60
+        const block7 = 80
+        const block8 = 100
+
+        let rewardA2 = 0
+        let rewardB2 = 0
+        let rewardC2 = 0
+        let rewardD2 = 0
+        let totalDeposits2 = 0
+
+        beforeEach(() => {
+            c.deposit(userA, amount1, block1)    // 500, 20
+            c.setExpectedReward(100, 22)
+            c.deposit(userB, amount2, block2)    // 700, 40
+            c.setExpectedReward(200, 60)
+            c.deposit(userC, amount3, block3)    // 200, 45
+            c.setExpectedReward(600, 30)
+            c.distribute(reward1, block4)        // 2000, 50
+
+            rewardA1 = c.userBalance(userA)
+            rewardB1 = c.userBalance(userB)
+            rewardC1 = c.userBalance(userC)
+            rewardD1 = c.userBalance(userD)
+            totalDeposits1 = c.getTotalDeposits()
+
+            c.setExpectedReward(700, 90)
+            c.withdraw(userC, amount5, block5)  // 200, 60
+            c.deposit(userC, amount6, block6)   // 200, 60
+            c.setExpectedReward(600, 65)
+            c.deposit(userD, amount7, block7)   // 1000, 80
+            c.setExpectedReward(1000, 120)
+            c.distribute(reward2, block8)    // 3000, 100
+
+            rewardA2 = c.userBalance(userA)
+            rewardB2 = c.userBalance(userB)
+            rewardC2 = c.userBalance(userC)
+            rewardD2 = c.userBalance(userD)
+            totalDeposits2 = c.getTotalDeposits()
+
+        })
+        test('sum of rewards after the first distribution is consistent',()=>{
+            expect(rewardA1 + rewardB1 + rewardC1 + rewardD1).toBeCloseTo(totalDeposits1, 8)
+            expect(rewardA1 + rewardB1 + rewardC1 + rewardD1).toBeCloseTo(amount1 + amount2 + amount3 + reward1, 8)
+        })
+        test('sum of rewards after the second distribution is consistent',()=>{
+            expect(rewardA2 + rewardB2 + rewardC2 + rewardD2).toBeCloseTo(totalDeposits2, 8)
+            expect(rewardA2 + rewardB2 + rewardC2 + rewardD2).toBeCloseTo(amount1 + amount2 + amount3 + reward1 - amount5 + amount6 + amount7 + reward2, 8)
         })
     })
 })
