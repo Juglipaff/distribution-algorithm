7db9fec25abb1d9ed81d58572d7bfb5fb927e3e7
Juglipaff <juglipaff@gmail.com>
2022-05-27 18:41:20 +0300

    refactor

--- contract.js
+++ contract.js
@@ -1,113 +1,99 @@
 module.exports = class Contract {
     constructor() {
-        this.stakes = {}
+        this.userDeposit = {}
         this.totalDeposits = 0
-        this.previosDeposit = 0
-        this.previosDepositForUser={}
-        this.K = 0
-        this.L = 0
-        this.lastDistribution = 0
-        this.lastDistributionForUser = {}
-        this.cumulativeBlockDeposits = 0
-        this.cumulativeBlockDepositsForUser = {}
-        this.nextDistribution={}
-        this.hasDeposited= {}
+        this.totalDepositLastUpdated = 0
+        this.userDepositLastUpdated={}
 
-        this.KForUser = {}
-        this.KOnBlock = {}
+        this.totalDepositAge = 0
+        this.userDepositAge = {}
+        this.userDepositChanged = {}
 
         this.rewards = {}
-        this.LOnBlock = {}
         this.cumulativeRewards = 0
+
+        this.distributionID = 0
+        this.distributionIDForUser = {}
+
+        this.K = {}
+        this.L = {}
+        this.distributions = {}
+    }
+    
+    _initJSVariables(user){
+        if(this.userDepositChanged[user]===undefined){
+            this.userDepositChanged[user] = {}
+        }
+        if(this.userDepositAge[user] === undefined){
+            this.userDepositAge[user] = 0
+            this.userDeposit[user] = 0
+            this.distributionIDForUser[user] = 0
+            this.userDepositLastUpdated[user] = 0
+            this.rewards[user] = 0
+        }
+    }
+
+    _updateDeposit(user, currentBlock) {
+        this._initJSVariables(user)
+
+        if(this.userDepositChanged[user][this.distributionID]){
+            this.userDepositAge[user] += this.userDeposit[user]*(currentBlock - this.userDepositLastUpdated[user])
+        }else{
+            this.rewards[user] += this.userReward(user) 
+            this.userDepositAge[user] = this.userDeposit[user]*(currentBlock - (this.distributions[this.distributionID - 1]||0))
+            this.userDepositChanged[user][this.distributionID] = true
+        }
+
+        this.totalDepositAge += this.totalDeposits*(currentBlock - this.totalDepositLastUpdated)
+
+        this.userDepositLastUpdated[user] = currentBlock
+        this.totalDepositLastUpdated = currentBlock
+        
+        this.distributionIDForUser[user] = this.distributionID
     }
 
     deposit(user, amount, currentBlock) {
-        if(this.hasDeposited[user]===undefined){
-            this.hasDeposited[user]={}
-        }
-        if(!this.hasDeposited[user][this.lastDistribution]){
-            this.hasDeposited[user][this.lastDistribution] = true
-            this.rewards[user] = (this.rewards[user]||0) + this.userReward(user)
+        this._updateDeposit(user, currentBlock)
 
-            this.cumulativeBlockDepositsForUser[user] = 0
-        }
-
-        this.cumulativeBlockDeposits += this.totalDeposits*(currentBlock - this.previosDeposit) 
-
-        const previousDepositBlock = ((this.previosDepositForUser[user]||0) < this.lastDistribution) ? this.lastDistribution : (this.previosDepositForUser[user]||0)
-        this.cumulativeBlockDepositsForUser[user] = (this.cumulativeBlockDepositsForUser[user]||0) + (this.stakes[user]||0)*(currentBlock - previousDepositBlock)
-
-        this.stakes[user] = (this.stakes[user]||0) + amount
+        this.userDeposit[user] += amount
         this.totalDeposits += amount
-
-        this.previosDepositForUser[user] = currentBlock
-        this.previosDeposit = currentBlock
-
-        this.lastDistributionForUser[user] = this.lastDistribution 
-        this.KForUser[user] = this.K 
     }
 
     withdraw(user, amount, currentBlock) {
-        if(!this.hasDeposited[user][this.lastDistribution]){
-            this.hasDeposited[user][this.lastDistribution] = true
-            this.rewards[user] += this.userReward(user) 
-            
-            this.cumulativeBlockDepositsForUser[user] = 0
-        }
-       
-        this.cumulativeBlockDeposits += this.totalDeposits*(currentBlock - this.previosDeposit)
-        
-        const previosDepositBlock = (this.previosDepositForUser[user] < this.lastDistribution) ? this.lastDistribution : this.previosDepositForUser[user]
-        this.cumulativeBlockDepositsForUser[user] += this.stakes[user]*(currentBlock - previosDepositBlock)
+        this._updateDeposit(user, currentBlock)
 
+        this.userDeposit[user] -= amount
         this.totalDeposits -= amount
-        this.stakes[user] -= amount
-
-        this.previosDepositForUser[user] = currentBlock
-        this.previosDeposit = currentBlock
-
-        this.lastDistributionForUser[user] = this.lastDistribution 
-        this.KForUser[user] = this.K 
     }
 
     distribute(reward, currentBlock) {
-        const _K = reward/(this.cumulativeBlockDeposits + this.totalDeposits*(currentBlock - this.previosDeposit))
-        this.K += _K
-        this.L += _K * (currentBlock - this.lastDistribution)
+        const _K = reward/(this.totalDepositAge + this.totalDeposits*(currentBlock - this.totalDepositLastUpdated))
 
-        this.nextDistribution[this.lastDistribution] = currentBlock
-        this.KOnBlock[this.lastDistribution] = this.K
-        this.LOnBlock[this.lastDistribution] = this.L
+        this.K[this.distributionID] = (this.K[this.distributionID - 1] || 0) + _K
+        this.L[this.distributionID] = (this.L[this.distributionID - 1] || 0) + _K * (currentBlock - (this.distributions[this.distributionID - 1]||0))
+        this.distributions[this.distributionID] = currentBlock
 
-        this.lastDistribution = currentBlock
-        this.previosDeposit = currentBlock
+        this.distributionID += 1
+
+        this.totalDepositLastUpdated = currentBlock
         this.cumulativeRewards += reward
-        this.cumulativeBlockDeposits = 0
+        this.totalDepositAge = 0
     }
 
     userReward(user) {
-        let nextDistributionForUser
-        if(this.lastDistributionForUser[user]===undefined || !this.nextDistribution[this.lastDistributionForUser[user]]){//not sure how to handle undefined in solidity since mappings have 0 as their default value
-            nextDistributionForUser = this.lastDistribution
+        this._initJSVariables(user)
+        if(this.distributionIDForUser[user] == this.distributionID){
+            return this.rewards[user]
         }else{
-            nextDistributionForUser = this.nextDistribution[this.lastDistributionForUser[user]]
+            const _userDepositAge = this.userDepositAge[user] + this.userDeposit[user] * ((this.distributions[this.distributionIDForUser[user]]||0) - this.userDepositLastUpdated[user])
+            const rewardsBeforeDistibution = _userDepositAge * ((this.K[this.distributionIDForUser[user]]||0) - (this.K[this.distributionIDForUser[user] - 1]||0))
+            const rewardsAfterDistribution = this.userDeposit[user] * ((this.L[this.distributionID - 1]||0) - (this.L[this.distributionIDForUser[user]]||0))
+            return this.rewards[user] + rewardsBeforeDistibution + rewardsAfterDistribution
         }
-
-        const _previosDepositForUser = (this.previosDepositForUser[user]||0) > nextDistributionForUser ? nextDistributionForUser : (this.previosDepositForUser[user]||0)
-        const _cumulativeBlockDepositsForUser = (this.cumulativeBlockDepositsForUser[user]||0) + (this.stakes[user]||0) * (nextDistributionForUser - _previosDepositForUser)
-        const nextK = this.KOnBlock[this.lastDistributionForUser[user]] === undefined ? this.K : this.KOnBlock[this.lastDistributionForUser[user]]//not sure how to handle undefined in solidity since mappings have 0 as their default value
-        const deltaK = nextK - (this.KForUser[user]||0)
-        const rewardsBeforeDistibution = _cumulativeBlockDepositsForUser * deltaK
-        
-        const nextL = this.LOnBlock[this.lastDistributionForUser[user]] === undefined ? this.L : this.LOnBlock[this.lastDistributionForUser[user]]//not sure how to handle undefined in solidity since mappings have 0 as their default value
-        const deltaLForUser = this.L - nextL
-        const rewardsAfterDistribution = (this.stakes[user]||0) * deltaLForUser
-
-        return (this.rewards[user]||0) + rewardsBeforeDistibution + rewardsAfterDistribution
     }
 
     userBalance(user,log) {
-        return this.stakes[user] + this.userReward(user,log)
+        return this.userDeposit[user] + this.userReward(user,log)
     }
 
     getTotalDeposits() {
